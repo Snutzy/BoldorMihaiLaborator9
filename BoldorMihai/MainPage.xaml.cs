317a06c6a4628b0b4689b10edd5822c4a8da25fc
Snutzy <mihaitzaboldor@gmail.com>
2024-01-08 19:03:21 +0200

    Add project files.

---
+++ BoldorMihai/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace BoldorMihai
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Ai apasat o data";
+            else
+                CounterBtn.Text = $"Ai apasat de {count} ori";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
